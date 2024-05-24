fab0a988955538b980ea00c821cdbf0abab94992
semanurgüçlü <smnrguclu06@gmail.com>
2024-05-24 21:43:45 +0300

    I removed user related DB operations from DbPort

--- internal/application/infrastructure/port/repository/db.go
+++ internal/application/infrastructure/port/repository/db.go
@@ -3,8 +3,6 @@ package application
 import (
 	"context"
 
-	"github.com/google/uuid"
-	me "github.com/octoposprime/op-be-auth/internal/domain/model/entity"
 	pb_logging "github.com/octoposprime/op-be-shared/pkg/proto/pb/logging"
 )
 
@@ -13,10 +11,4 @@ import (
 type DbPort interface {
 	// SetLogger sets logging call-back function
 	SetLogger(LogFunc func(ctx context.Context, logData *pb_logging.LogData) (*pb_logging.LoggingResult, error))
-
-	// GetUsersByFilter returns the users that match the given filter.
-	GetUsersByFilter(ctx context.Context, userFilter me.UserFilter) (me.Users, error)
-
-	// GetUserPasswordByUserId returns active password of the given user.
-	GetUserPasswordByUserId(ctx context.Context, userId uuid.UUID) (me.UserPassword, error)
 }
